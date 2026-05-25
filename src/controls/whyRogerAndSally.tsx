6516f2b4a47066e6e42631af8637164def595b33
Roger Wyatt <rogergwyatt@gmail.com>
2026-05-25 13:44:12 -0400

    Removing unneeded assets

--- src/controls/whyRogerAndSally.tsx
+++ src/controls/whyRogerAndSally.tsx
@@ -45,6 +45,28 @@ export default function WhyRogerAndSally() {
                         />
                     </div>
                 </div>
+                <div className="mt-5 text-left">
+                    The Heritage Lock sets a Roger and Sally board apart from everyone else. We use dowel joinery that provides an perpendicular grain direction and is bonded to the interior of the board. 
+                    This technique not only looks amazing, it provides a couple of structural benefits. First, it bonds the outside strips together which prevents splits. 
+                    Second, it helps control wood movement
+                </div>
+                <div className="mt-5 text-left">
+                    <p>Our Signature Lock is brand new. We create a sliding dovetail on each strip that makes up the board. This means that not only are the strips glue bonded and bonded with The Heritage Lock, 
+                    they are bonded completely mechanically. This joinery makes it impossible for the board to separate. The wood would have to break before the strips coule separate. 
+                    </p>
+                    <p className="mt-2 text-left mb-10">
+                    The Signature Lock is only available on Signature series boards.
+                    </p>
+                </div>
+                <div>
+                        <Image
+                        className="z-0 overflow-hidden relative w-svw h-auto"
+                        src="/images/DSC_0050.jpg"
+                        alt="Reasons to choose Roger and Sally"
+                        width="1025"
+                        height="1025"
+                        />
+                </div>
             </div>
       </div>
     )
