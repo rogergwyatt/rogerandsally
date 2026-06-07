47e868949a3b2249d80e5be1bcf71b90820b2747
Roger Wyatt <rogergwyatt@gmail.com>
2026-06-07 17:16:42 -0400

    Adding more products and fixing up catalog

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -36,7 +36,7 @@ export default function ShopPage() {
         </div>
         <DropsSection />
 
-        <h2 className={`text-2xl lg:text-3xl text-walnut text-center mb-6 ${serif.className}`}>Our Catalog</h2>
+        <h2 className={`text-2xl lg:text-3xl text-walnut text-center mb-6 ${serif.className}`}>Made-To-Order Boards</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {(products as Product[]).map(product => (
             <Link
